cc7b98a542b2ec0ba76c06dc99cba93b618afd7a
Tiberiu Tesileanu <ttesileanu@gmail.com>
2023-07-09 20:38:07 -0400

    Remove old supervised NSM code

--- pynsm/arch/__init__.py
+++ pynsm/arch/__init__.py
@@ -2,4 +2,3 @@ from .base import IterationModule, IterationLossModule
 
 from .similarity import SimilarityMatching, MultiSimilarityMatching
 from .similarity import SupervisedSimilarityMatching
-from .sup_nsm_conv import Supervised_NSM_Conv
